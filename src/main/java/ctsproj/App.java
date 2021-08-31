7f648156b0a9d60a0682d871caa617cb1c3f57d7
troy <jp240197@gamil.com>
2021-08-31 08:46:57 +0530

    this is a file

---
+++ src/main/java/ctsproj/App.java
@@ -0,0 +1,13 @@
+package ctsproj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
